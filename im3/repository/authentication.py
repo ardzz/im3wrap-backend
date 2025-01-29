d22d2a78cccfa492dfcb2ec47fc74f492070e6d3
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2025-01-29 14:51:41 +0700

    chore(repository): add debug mode on authentication.py

--- im3/repository/authentication.py
+++ im3/repository/authentication.py
@@ -2,16 +2,16 @@ from im3 import IMIOAuthHelper
 
 
 class Authentication:
-    def __init__(self, phone_number):
+    def __init__(self, phone_number, debug=False):
         token_id = IMIOAuthHelper.get_values(IMIOAuthHelper.make_imi_uid())
-        self.api_service_instance = IMIOAuthHelper("Android", "82.5.1", token_id)
+        self.api_service_instance = IMIOAuthHelper("Android", "82.5.1", token_id, debug=debug)
         self.phone_number = self.parse_number(phone_number)
 
     def send_otp(self):
         return self.api_service_instance.send_request("api/v2/otp/send/v1",{"msisdn": self.phone_number, "action": "register"})
 
     def verify_otp(self, transid, code):
-        return self.api_service_instance.send_request("api/v2/otp/validate/v1", {"transid": transid, "otp": code})
+        return self.api_service_instance.send_request("api/v2/otp/validate/v1", {"transid":transid,"otp":str(code)})
 
     @staticmethod
     def parse_number(number):
