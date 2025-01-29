be9f98c87114c5a4fe789a494dcc1847379dbf44
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2025-01-29 23:30:50 +0700

    chore(docker-compose): refactor the celery config

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ from database import db
 from middleware.auth_middleware import auth_middleware
 from models.user import User
 from models.package import Package
-from routes.auth_route import auth_routes
-from routes.profile_route import user_routes
-from routes.im3_route import im3_routes
+from models.transaction import Transaction
 
 load_dotenv()
 
@@ -25,33 +23,39 @@ def create_app():
         CELERY_TASK_SEND_SENT_EVENT=True,
         CELERY_ENABLE_UTC=True,
         SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL', 'postgresql+psycopg2://myuser:mypassword@db:5432/im3wrap'),
+        JWT_SECRET_KEY=os.getenv('JWT_SECRET', 'jwt-super-secret-key'),
+        WTF_CSRF_ENABLED=False
     )
 
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET', 'jwt-super-secret-key')
-    app.config['WTF_CSRF_ENABLED'] = False
-
-    app.register_blueprint(auth_routes, url_prefix='/auth')
-    app.register_blueprint(user_routes, url_prefix='/user')
-    app.register_blueprint(im3_routes, url_prefix='/im3')
-
-    celery = Celery(
-        app.import_name,
-        broker=app.config['CELERY_BROKER_URL'],
-        backend=app.config['CELERY_RESULT_BACKEND'],
-    )
-    celery.conf.update(app.config)
-
+    # Middleware
     exclude_routes = ['auth_routes.login', 'auth_routes.register']
     app = auth_middleware(exempt_routes=exclude_routes)(app)
 
+    # Initialize extensions
     db.init_app(app)
-    migrate = Migrate(app, db)
-    jwt = JWTManager(app)
+    Migrate(app, db)
+    JWTManager(app)
 
-    return app, celery
+    with app.app_context():
+        from routes.auth_route import auth_routes
+        from routes.profile_route import user_routes
+        from routes.im3_route import im3_routes
+
+        app.register_blueprint(auth_routes, url_prefix='/auth')
+        app.register_blueprint(user_routes, url_prefix='/user')
+        app.register_blueprint(im3_routes, url_prefix='/im3')
+
+    celery = Celery(app.import_name,
+                    broker=app.config['CELERY_BROKER_URL'],
+                    include=['tasks.package_transaction'])  # Add this line
+    celery.conf.update(app.config)
+    app.celery = celery
+
+    return app
 
 
-app, celery = create_app()
+app = create_app()
+celery = app.celery
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=os.getenv('FLASK_DEBUG', False))
