8288d24c864e6bae2a425b708f57deb2f1d2270d
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2025-01-29 23:33:01 +0700

    feat(routes): add package routes

--- routes/im3_route.py
+++ routes/im3_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from controllers.im3_auth import IM3Auth
+from controllers.im3_package import IM3Package
 
 im3_routes = Blueprint('im3_routes', __name__)
 
@@ -17,3 +18,13 @@ def verify_otp():
 @im3_routes.route('/profile', methods=['GET'])
 def get_profile():
     return IM3Auth.get_profile()
+
+
+@im3_routes.route('/package', methods=['GET'])
+def get_package():
+    return IM3Package.get_package()
+
+
+@im3_routes.route('/package/buy', methods=['POST'])
+def buy_package():
+    return IM3Package.purchase_package()
